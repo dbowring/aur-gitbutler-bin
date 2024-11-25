09e98e6176f7d9c502b28b907f1ac6467875cf1f
Daniel Bowring <aur@daniel.bowring.email>
2024-11-26 10:47:50 +1100

    Reset pkgrel in update script

--- update.bash
+++ update.bash
@@ -9,9 +9,10 @@ VER=$(echo $URL | grep -oE '[0-9]+.[0-9]+.[0-9]+-' | sed 's/-//g')
 echo $VER
 echo $BUILD
 
+sed -i "s/pkgrel=.*/pkgrel=1/" PKGBUILD
 sed -i "s/pkgver=.*/pkgver=$VER/" PKGBUILD
 sed -i "s/_pkgvernum=.*/_pkgvernum=$BUILD/" PKGBUILD
 updpkgsums
 makepkg --printsrcinfo > .SRCINFO
-git add -p
-git commit -m "v$VER"
+#git add -p
+#git commit -m "v$VER"
